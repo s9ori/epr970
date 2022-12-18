b0a70637776658f1c65fe8026418a7f1b12f9ee2
s9ori <sebauchida@gmail.com>
2022-12-18 13:47:50 -0500

    todo app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+  class TodoApp extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = { items: [], text: '' };
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+  
+    componentDidMount() {
+      // Load the to-do list from local storage
+      const items = JSON.parse(localStorage.getItem('items'));
+      if (items) {
+        this.setState({ items });
+      }
+    }
+  
+    componentDidUpdate() {
+      // Save the to-do list to local storage
+      localStorage.setItem('items', JSON.stringify(this.state.items));
+    }
+  
+    render() {
+      return (
+        <div>
+          <h3>TODO</h3>
+          <TodoList items={this.state.items} />
+          <form onSubmit={this.handleSubmit}>
+            <label htmlFor="new-todo">
+              What needs to be done?
+            </label>
+            <input
+              id="new-todo"
+              onChange={this.handleChange}
+              value={this.state.text}
+            />
+            <button>
+              Add #{this.state.items.length + 1}
+            </button>
+          </form>
+        </div>
+      );
+    }
+    handleChange(e) {
+      this.setState({ text: e.target.value });
+    }
+  
+    handleSubmit(e) {
+      e.preventDefault();
+      if (this.state.text.length === 0) {
+        return;
+      }
+      const newItem = {
+        text: this.state.text,
+        id: Date.now()
+      };
+      this.setState(state => ({
+        items: state.items.concat(newItem),
+        text: ''
+      }));
+    }
+  }
+  
+  class TodoList extends React.Component {
+    render() {
+      return (
+        <ul>
+          {this.props.items.map(item => (
+            <li key={item.id}>{item.text}</li>
+          ))}
+        </ul>
+      );
+    }
+  }
+export default TodoApp;
