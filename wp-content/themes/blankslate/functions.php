da4e544e0974cf792b5929f842fb83ca5c21d555
s9ori <sebauchida@gmail.com>
2022-12-18 03:19:19 -0500

    f

--- wp-content/themes/blankslate/functions.php
+++ wp-content/themes/blankslate/functions.php
@@ -155,12 +155,4 @@ function blankslate_custom_pings( $comment ) {
 }
 add_filter( 'get_comments_number', 'blankslate_comment_count', 0 );
 function blankslate_comment_count( $count ) {
-if ( !is_admin() ) {
-global $id;
-$get_comments = get_comments( 'status=approve&post_id=' . $id );
-$comments_by_type = separate_comments( $get_comments );
-return count( $comments_by_type['comment'] );
-} else {
-return $count;
-}
 }
