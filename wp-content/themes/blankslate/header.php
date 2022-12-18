f2971e067b3c4712a8572ea7695087cd4b64cba9
s9ori <sebauchida@gmail.com>
2022-12-17 21:08:30 -0500

    new fonts

--- wp-content/themes/blankslate/header.php
+++ wp-content/themes/blankslate/header.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?> <?php blankslate_schema_type(); ?>>
 <head>
+<link rel="stylesheet" href="https://use.typekit.net/qzj8qus.css">
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width" />
 <?php wp_head(); ?>
