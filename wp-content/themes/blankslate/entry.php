56e65bdbf7bbd5145dd76f0bd297a82b69a8d0fe
s9ori <sebauchida@gmail.com>
2022-12-18 01:05:02 -0500

    w

--- wp-content/themes/blankslate/entry.php
+++ wp-content/themes/blankslate/entry.php
@@ -1,17 +1,5 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-<?php if ( is_singular() ) { echo '<h1 class="entry-title" itemprop="headline">'; } else { echo '<h2 class="entry-title">'; } ?>
-<!--<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark"><?php the_title(); ?></a>
-<?php if ( is_singular() ) { echo '</h1>'; } else { echo '</h2>'; } ?>
-<?php edit_post_link(); ?>
-<?php if ( !is_search() ) { get_template_part( 'entry', 'meta' ); } ?>
-<?php get_template_part( 'entry', ( is_front_page() || is_home() || is_front_page() && is_home() || is_archive() || is_search() ? 'summary' : 'content' ) ); ?>
-<?php if ( is_singular() ) { get_template_part( 'entry-footer' ); } ?>
-<nav id="menu">
-        <div id="search"><?php /* get_search_form(); */ ?></div>
-        <!-- This needs to remain in the code in order to render the menu! -->
-        <?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
-    </nav>
-    <div class="hold">
+<div class="hold">
 <div id="branding">
   <a href="/">
     <img class="head-image" src="wp-content/themes/blankslate/images/matissespe.png">
@@ -26,4 +14,16 @@
 <div id="wrapper" class="hfeed">
 <div id="container">
 <main id="content" role="main">
+<?php if ( is_singular() ) { echo '<h1 class="entry-title" itemprop="headline">'; } else { echo '<h2 class="entry-title">'; } ?>
+<!--<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark"><?php the_title(); ?></a>
+<?php if ( is_singular() ) { echo '</h1>'; } else { echo '</h2>'; } ?>
+<?php edit_post_link(); ?>
+<?php if ( !is_search() ) { get_template_part( 'entry', 'meta' ); } ?>
+<?php get_template_part( 'entry', ( is_front_page() || is_home() || is_front_page() && is_home() || is_archive() || is_search() ? 'summary' : 'content' ) ); ?>
+<?php if ( is_singular() ) { get_template_part( 'entry-footer' ); } ?>
+<nav id="menu">
+        <div id="search"><?php /* get_search_form(); */ ?></div>
+        This needs to remain in the code in order to render the menu!
+        <?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
+    </nav>-->
 </article>
