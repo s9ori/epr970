a26454f4a91ca05ad2a7fcd78dd07487e5e30b87
s9ori <sebauchida@gmail.com>
2022-12-18 03:03:42 -0500

    updates

--- wp-content/themes/blankslate/header.php
+++ wp-content/themes/blankslate/header.php
@@ -2,6 +2,8 @@
 <html <?php language_attributes(); ?> <?php blankslate_schema_type(); ?>>
 <head>
 <link rel="stylesheet" href="https://use.typekit.net/qzj8qus.css">
+<script src="wp-content/themes/blankslate/magnet-mouse.js"></script>
+<script scr="wp-content/themes/blankslate/magnetic-border.js"></script>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width" />
 <?php wp_head(); ?>
