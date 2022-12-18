230b7a406ad1e36b21dc0ac11c485551063106c2
s9ori <sebauchida@gmail.com>
2022-12-18 02:40:56 -0500

    magnet

--- wp-content/themes/blankslate/magnetic-border.js
+++ wp-content/themes/blankslate/magnetic-border.js
@@ -1,7 +1,8 @@
-document.querySelector('.entry-content').onmousemove = (e) => {
-  const x = e.pageX - e.target.offsetLeft
-  const y = e.pageY - e.target.offsetTop
-
-  e.target.style.setProperty('--x', `${ x }px`)
-  e.target.style.setProperty('--y', `${ y }px`)
-}
+let mm = new MagnetMouse({
+    magnet: {
+      element: '.magnet'
+    }
+  });
+  
+  mm.init();
+  
