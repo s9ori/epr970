0101b6db979e39062f1b665c8ee4ab769f9a88be
s9ori <sebauchida@gmail.com>
2022-12-18 01:58:27 -0500

    new border

--- wp-content/themes/blankslate/magnetic-border.js
+++ wp-content/themes/blankslate/magnetic-border.js
@@ -7,4 +7,7 @@ element.addEventListener('mousemove', (event) => {
   const height = element.offsetHeight;
 
   element.style.transform = `translate(${(x - width / 35) / (width / 35)}px, ${(y - height / 35) / (height / 35)}px)`;
+  element.style.transition = 'transform 0.5s';
+  element.style.pointerEvents = 'none';
+  element.style.clipPath = `circle(50px at ${x}px ${y}px)`;
 });
