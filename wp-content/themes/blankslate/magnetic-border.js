ed8cdd1a83f6a885bec9f25dd978dc4da0864cdd
s9ori <sebauchida@gmail.com>
2022-12-18 02:11:36 -0500

    new magnetc

--- wp-content/themes/blankslate/magnetic-border.js
+++ wp-content/themes/blankslate/magnetic-border.js
@@ -1,12 +1,7 @@
-const element = document.querySelector('.entry-content');
+document.querySelector('.entry-content').onmousemove = (e) => {
+  const x = e.pageX - e.target.offsetLeft
+  const y = e.pageY - e.target.offsetTop
 
-element.addEventListener('mousemove', (event) => {
-  const x = event.clientX;
-  const y = event.clientY;
-  const width = element.offsetWidth;
-  const height = element.offsetHeight;
-  const distance = Math.sqrt(Math.pow(x - width / 2, 2) + Math.pow(y - height / 2, 2));
-
-  element.style.transform = `translate(${(x - width / 2) / (distance / 15)}px, ${(y - height / 2) / (distance / 15)}px)`;
-  element.style.pointerEvents = 'none';
-});
+  e.target.style.setProperty('--x', `${ x }px`)
+  e.target.style.setProperty('--y', `${ y }px`)
+}
