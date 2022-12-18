b54eed60dfc135a0a09dc9fc6bc5dc3374ed7cb1
s9ori <sebauchida@gmail.com>
2022-12-18 02:47:46 -0500

    magnet

--- wp-content/themes/blankslate/magnetic-border.js
+++ wp-content/themes/blankslate/magnetic-border.js
@@ -1,7 +1,12 @@
 let mm = new MagnetMouse({
     magnet: {
-      element: '.magnet'
-    }
+      element: '.entry-content',
+      class: 'magnet-active',
+      enabled: true,
+      distance: 40,
+      position: 'bottom-right'
+    },
+    // Other configuration options
   });
   
   mm.init();
