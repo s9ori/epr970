b8e80a7ee0f42efb84b7c3255fb7521e81ffa717
s9ori <sebauchida@gmail.com>
2022-12-18 19:23:19 -0500

    plugin

--- wp-content/plugins/brads-boilerplate-block-plugin/src/index.js
+++ wp-content/plugins/brads-boilerplate-block-plugin/src/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./index.scss"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+wp.blocks.registerBlockType("makeupnamespace/make-up-block-name", {
+  title: "Brads Boilerplate Block",
+  icon: "welcome-learn-more",
+  category: "common",
+  attributes: {
+    skyColor: { type: "string" },
+    grassColor: { type: "string" }
+  },
+  edit: EditComponent,
+  save: function () {
+    return null
+  }
+})
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function EditComponent(props) {
+  function updateSkyColor(e) {
+    props.setAttributes({ skyColor: e.target.value })
+  }
+
+  function updateGrassColor(e) {
+    props.setAttributes({ grassColor: e.target.value })
+  }
+
+  return (
+    <div className="makeUpYourBlockTypeName">
+      <input type="text" value={props.attributes.skyColor} onChange={updateSkyColor} placeholder="sky color..." />
+      <input type="text" value={props.attributes.grassColor} onChange={updateGrassColor} placeholder="grass color..." />
+    </div>
+  )
+}
