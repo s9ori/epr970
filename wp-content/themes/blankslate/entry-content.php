b71f08f339535c9b80a4526540976c91c21d9d9f
s9ori <sebauchida@gmail.com>
2022-12-20 17:44:43 -0500

    update

--- wp-content/themes/blankslate/entry-content.php
+++ wp-content/themes/blankslate/entry-content.php
@@ -1,19 +1,4 @@
-<div class="hold">
-<div id="branding">
-  <a href="/">
-    <img class="head-image" src="wp-content/themes/blankslate/images/matissespe.png">
-  </a>
-</div>
-<div class="title1">
-  <a href="/">
-    <h1>personal website</h1>
-  </a>
-</div>
-</div>
-<div id="wrapper" class="hfeed">
-<div id="container">
-<main id="content" role="main">
-    <div class="entry-content" itemprop="mainEntityOfPage">
+<div class="entry-content" itemprop="mainEntityOfPage">
 <?php if ( has_post_thumbnail() ) : ?>
 <a href="<?php the_post_thumbnail_url( 'full' ); ?>" title="<?php $attachment_id = get_post_thumbnail_id( $post->ID ); the_title_attribute( array( 'post' => get_post( $attachment_id ) ) ); ?>"><?php the_post_thumbnail( 'full', array( 'itemprop' => 'image' ) ); ?></a>
 <?php endif; ?>
--- wp-content/themes/blankslate/magnetic-border.js
+++
@@ -1,8 +0,0 @@
-let mm = new MagnetMouse({
-    magnet: {
-      element: '.entry-content',
-      position: 'center',
-      distance: 30
-    }
-  });
-  mm.init();
