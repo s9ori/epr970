885401e47df20954a5009c74930fbaa36db5f789
s9ori <sebauchida@gmail.com>
2022-12-20 17:25:30 -0500

    entry

--- wp-content/themes/blankslate/entry.php
+++ wp-content/themes/blankslate/entry.php
@@ -1,6 +1,6 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 <?php if ( is_singular() ) { echo '<h1 class="entry-title" itemprop="headline">'; } else { echo '<h2 class="entry-title">'; } ?>
-<!--<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark"><?php the_title(); ?></a>
+<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark"><?php the_title(); ?></a>
 <?php if ( is_singular() ) { echo '</h1>'; } else { echo '</h2>'; } ?>
 <?php edit_post_link(); ?>
 <?php if ( !is_search() ) { get_template_part( 'entry', 'meta' ); } ?>
@@ -10,5 +10,5 @@
         <div id="search"><?php /* get_search_form(); */ ?></div>
         This needs to remain in the code in order to render the menu!
         <?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
-    </nav>-->
+    </nav>
 </article>
