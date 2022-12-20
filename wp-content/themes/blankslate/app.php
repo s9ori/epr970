1adea014da42fc99c87f95cfee30c456c5dd92f8
s9ori <sebauchida@gmail.com>
2022-12-19 22:18:17 -0500

    3 Please enter the commit message for your changes. Lines starting

---
+++ wp-content/themes/blankslate/app.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Template Name: React App
+*/
+get_header();
+?>
+
+<div id="root"></div>
+
+<script src="<?php echo get_template_directory_uri(); ?>/static/js/main.js"></script>
+
+<?php
+get_footer();
