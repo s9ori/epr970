26ee66a408ce6c28fa21c833fe5324d563f81c26
s9ori <sebauchida@gmail.com>
2022-12-20 22:07:42 -0500

    entry basics

--- wp-content/themes/blankslate/entry.php
+++ wp-content/themes/blankslate/entry.php
@@ -1,4 +1,5 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<?php get_template_part( 'nav', 'below-single' ); ?>
 <div class="entry-basics">
 <?php if ( is_singular() ) { echo '<h1 class="entry-title" itemprop="headline">'; } else { echo '<h2 class="entry-title">'; } ?>
 <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark"><?php the_title(); ?></a>
