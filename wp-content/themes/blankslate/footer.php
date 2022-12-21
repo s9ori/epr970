ca20d5b6cc2969117adcb554e8c2c62b6c625f1a
s9ori <sebauchida@gmail.com>
2022-12-21 10:45:57 -0500

    changes

--- wp-content/themes/blankslate/footer.php
+++ wp-content/themes/blankslate/footer.php
@@ -35,34 +35,5 @@ tab.addEventListener('click', () => {
 
 </script>
 
-<div class="latest-post-block-container">
-  <?php
-    // Retrieve the latest post
-    $latest_post = get_posts( array(
-      'numberposts' => 1,
-      'orderby' => 'post_date',
-      'order' => 'DESC',
-      'post_type' => 'post',
-      'post_status' => 'publish'
-    ) );
-
-    // Get the featured image URL
-    $featured_image_url = get_the_post_thumbnail_url( $latest_post[0]->ID );
-    // Get the post title
-    $title = $latest_post[0]->post_title;
-  ?>
-
-  <!-- Use the values in the HTML structure -->
-  <div class="latest-post-block">
-    <div class="latest-post-block__featured-image-container">
-      <img src="<?php echo $featured_image_url; ?>" alt="Featured image" class="latest-post-block__featured-image">
-    </div>
-    <div class="latest-post-block__title-container">
-      <h3 class="latest-post-block__title"><?php echo $title; ?></h3>
-    </div>
-  </div>
-</div>
-
-
 </body>
 </html>
