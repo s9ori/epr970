89f2fdcf42f10ed4df120daaf00222cf6c28db9c
s9ori <sebauchida@gmail.com>
2022-12-20 20:08:34 -0500

    post columns

--- wp-content/themes/blankslate/page.php
+++ wp-content/themes/blankslate/page.php
@@ -1,9 +1,9 @@
 <?php get_header(); ?>
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-<header class="header">
+<div class="entry-basics">
 <h1 class="entry-title" itemprop="name"><?php the_title(); ?></h1> <?php edit_post_link(); ?>
-</header>
+</div>
 <div class="entry-content" itemprop="mainContentOfPage">
 <?php if ( has_post_thumbnail() ) { the_post_thumbnail( 'full', array( 'itemprop' => 'image' ) ); } ?>
 <?php the_content(); ?>
