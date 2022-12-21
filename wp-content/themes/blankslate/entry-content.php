c141caa1ce2b75ce9c2a25ecd1977520881d4d48
s9ori <sebauchida@gmail.com>
2022-12-21 00:17:33 -0500

    links

--- wp-content/themes/blankslate/entry-content.php
+++ wp-content/themes/blankslate/entry-content.php
@@ -4,5 +4,4 @@
 <?php endif; ?>
 <meta itemprop="description" content="<?php echo esc_html( wp_strip_all_tags( get_the_excerpt(), true ) ); ?>" />
 <?php the_content(); ?>
-<div class="entry-links"><?php wp_link_pages(); ?></div>
 </div>
