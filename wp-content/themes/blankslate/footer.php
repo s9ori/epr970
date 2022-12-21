442070c218e4418ac3d02e6d73ce43452e652e05
s9ori <sebauchida@gmail.com>
2022-12-20 21:23:56 -0500

    footer

--- wp-content/themes/blankslate/footer.php
+++ wp-content/themes/blankslate/footer.php
@@ -2,7 +2,6 @@
 <?php get_sidebar(); ?>
 </div>
 <footer id="footer" role="contentinfo">
-<div id="copyright">
 &copy; <?php echo esc_html( date_i18n( __( 'Y', 'blankslate' ) ) ); ?> <?php echo esc_html( get_bloginfo( 'name' ) ); ?>
 </div>
 </footer>
