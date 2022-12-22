4745179c33131e27a1d1dc22d1fc29aac2745d0d
s9ori <sebauchida@gmail.com>
2022-12-22 18:45:19 -0500

    cvd

--- wp-content/themes/blankslate/header.php
+++ wp-content/themes/blankslate/header.php
@@ -44,7 +44,8 @@ if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo ''; }
     ) );
 
     // Loop through the posts
-    foreach ( $latest_post as $post ) {
+    for ( $i = 1; $i < 3; $i++ ) {
+      $post = $latest_posts[$i];
       // Get the featured image URL
       $featured_image_url = get_the_post_thumbnail_url( $post->ID );
       // Get the post title
