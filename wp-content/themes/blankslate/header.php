910d0ff55e88ff9f20acf25a6c80c293c6f243c5
s9ori <sebauchida@gmail.com>
2022-12-22 02:33:42 -0500

    styles

--- wp-content/themes/blankslate/header.php
+++ wp-content/themes/blankslate/header.php
@@ -10,7 +10,6 @@
 <div id="wrapper" class="hfeed">
 <header id="header" role="banner">
 <div id="branding">
-    <img class="logo" src="http://0nx.17b.myftpupload.com/wp-content/uploads/2022/12/cropped-DALL·E-2022-12-17-22.21.36-a-lady-dior-hand-bag-a-journal-and-a-lip-stick-on-a-table-oil-painting-by-matisse.png">
 <div id="site-title" itemprop="publisher" itemscope itemtype="https://schema.org/Organization">
 <?php
 if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo ''; }
@@ -19,6 +18,7 @@ if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo ''; }
 ?>
 </div>
 <div id="site-description"<?php if ( !is_single() ) { echo ' itemprop="description"'; } ?>><?php bloginfo( 'description' ); ?></div>
+<img class="logo" src="http://0nx.17b.myftpupload.com/wp-content/uploads/2022/12/cropped-DALL·E-2022-12-17-22.21.36-a-lady-dior-hand-bag-a-journal-and-a-lip-stick-on-a-table-oil-painting-by-matisse.png">
 </div>
 <nav id="menu" role="navigation" itemscope itemtype="https://schema.org/SiteNavigationElement">
 <?php wp_nav_menu( array( 'theme_location' => 'main-menu', 'link_before' => '<span itemprop="name">', 'link_after' => '</span>' ) ); ?>
