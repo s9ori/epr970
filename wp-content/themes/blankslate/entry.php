82caf52e8ece564320eff3365475b8f058e28d37
s9ori <sebauchida@gmail.com>
2022-12-22 19:28:31 -0500

    update

--- wp-content/themes/blankslate/entry.php
+++ wp-content/themes/blankslate/entry.php
@@ -1,4 +1,38 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<div class="latest-post-block-container">
+  <?php
+    // Retrieve the latest post
+    $latest_post = get_posts( array(
+      'numberposts' => 1,
+      'orderby' => 'post_date',
+      'order' => 'DESC',
+      'post_type' => 'post',
+      'post_status' => 'publish'
+    ) );
+
+    // Loop through the posts
+    foreach ( $latest_post as $post ) {
+      // Get the featured image URL
+      $featured_image_url = get_the_post_thumbnail_url( $post->ID );
+      // Get the post title
+      $title = $post->post_title;
+      // Get the post URL
+      $post_url = get_permalink( $post->ID );
+
+      // Output the HTML structure for each post
+      ?>
+      <a href="<?php echo $post_url; ?>" class="latest-post-block-link">
+        <div class="latest-post-block">
+          <div class="latest-post-block__featured-image-container">
+            <img src="<?php echo $featured_image_url; ?>" alt="Featured image" class="latest-post-block__featured-image">
+          </div>
+          <div class="latest-post-block__title-container">
+            <h3 class="latest-post-block__title"><?php echo $title; ?></h3>
+          </div>
+        </div>
+      </a>
+    <?php } ?>
+</div>
 <div class="tab"></div>
 <?php get_template_part( 'nav', 'below-single' ); ?>
 <div class="entry-basics">
