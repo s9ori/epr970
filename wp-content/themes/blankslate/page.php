9846d72a2221e1f46f2bfce12ae1805f382a2b6e
s9ori <sebauchida@gmail.com>
2022-12-22 22:29:33 -0500

    0o9

--- wp-content/themes/blankslate/page.php
+++ wp-content/themes/blankslate/page.php
@@ -9,6 +9,40 @@
 <?php the_content(); ?>
 </div>
 </article>
+<div class="latest-post-block-container">
+  <?php
+    // Retrieve the most recent post
+    $latest_post = get_posts( array(
+      'numberposts' => 1,
+      'orderby' => 'post_date',
+      'order' => 'DESC',
+      'post_type' => 'post',
+      'post_status' => 'publish'
+    ) );
+
+    // Loop through the posts
+    foreach ( $latest_post as $post ) {
+      // Get the featured image URL
+      $featured_image_url = get_the_post_thumbnail_url( $post->ID );
+      // Get the post title
+      $title = $post->post_title;
+      // Get the post URL
+      $post_url = get_permalink( $post->ID );
+
+      // Output the HTML structure for the post
+      ?>
+      <a href="<?php echo $post_url; ?>" class="latest-post-block-link">
+        <div class="latest-post-block">
+          <div class="latest-post-block__featured-image-container">
+            <img src="<?php echo $featured_image_url; ?>" alt="Featured image" class="latest-post-block__featured-image">
+          </div>
+          <div class="latest-post-block__title-container">
+            <h3 class="latest-post-block__title"><?php echo $title; ?></h3>
+          </div>
+        </div>
+      </a>
+    <?php } ?>
+</div>
 <?php if ( comments_open() && !post_password_required() ) { comments_template( '', true ); } ?>
 <?php endwhile; endif; ?>
 <?php get_footer(); ?>
