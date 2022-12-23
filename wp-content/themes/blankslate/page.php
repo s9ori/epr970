84eabc5c14b43714ca46fc0ab146dfd320ee6d5d
s9ori <sebauchida@gmail.com>
2022-12-22 22:55:57 -0500

    homepage

--- wp-content/themes/blankslate/page.php
+++ wp-content/themes/blankslate/page.php
@@ -1,40 +1,6 @@
-<!--Pages, including hp-->
+<!--Pages-->
 <?php get_header(); ?>
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-<div class="latest-post-block-container">
-  <?php
-    // Retrieve the most recent post
-    $latest_post = get_posts( array(
-      'numberposts' => 3,
-      'orderby' => 'post_date',
-      'order' => 'DESC',
-      'post_type' => 'post',
-      'post_status' => 'publish'
-    ) );
-
-    // Loop through the posts
-    foreach ( $latest_post as $post ) {
-      // Get the featured image URL
-      $featured_image_url = get_the_post_thumbnail_url( $post->ID );
-      // Get the post title
-      $title = $post->post_title;
-      // Get the post URL
-      $post_url = get_permalink( $post->ID );
-
-      // Output the HTML structure for the post
-      ?>
-      <a href="<?php echo $post_url; ?>" class="latest-post-block-link">
-        <div class="latest-post-block">
-          <div class="latest-post-block__featured-image-container">
-            <img src="<?php echo $featured_image_url; ?>" alt="Featured image" class="latest-post-block__featured-image">
-          </div>
-          <div class="latest-post-block__title-container">
-            <h3 class="latest-post-block__title"><?php echo $title; ?></h3>
-          </div>
-        </div>
-      </a>
-    <?php } ?>
-</div>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 <div class="entry-basics">
 <h1 class="entry-title" itemprop="name"><?php the_title(); ?></h1> <?php edit_post_link(); ?>
