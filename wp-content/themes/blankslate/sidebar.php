fc2d68603208599216815977a23e87b47090f4d9
s9ori <sebauchida@gmail.com>
2022-12-22 23:09:20 -0500

    sidebar

--- wp-content/themes/blankslate/sidebar.php
+++ wp-content/themes/blankslate/sidebar.php
@@ -1,10 +1,9 @@
-<div id="subcontainer">
+<?php if ( is_active_sidebar( 'primary-widget-area' ) ) : ?>
 <aside id="sidebar" role="complementary">
 <div id="primary" class="widget-area">
 <ul class="xoxo">
 <?php dynamic_sidebar( 'primary-widget-area' ); ?>
 </ul>
 </div>
-</div>
-
 </aside>
+<?php endif; ?>
