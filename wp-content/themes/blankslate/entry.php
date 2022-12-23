9452f3a80affa9f8c2b9c5d42689207d3653f980
s9ori <sebauchida@gmail.com>
2022-12-22 22:33:58 -0500

    commit

--- wp-content/themes/blankslate/entry.php
+++ wp-content/themes/blankslate/entry.php
@@ -1,3 +1,4 @@
+<!--POST PAGE-->
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 <div class="tab"></div>
 <?php get_template_part( 'nav', 'below-single' ); ?>
@@ -11,37 +12,3 @@
 <?php get_template_part( 'entry', ( is_front_page() || is_home() || is_front_page() && is_home() || is_archive() || is_search() ? 'summary' : 'content' ) ); ?>
 <?php if ( is_singular() ) { get_template_part( 'entry-footer' ); } ?>
 </article>
-<div class="latest-post-block-container">
-  <?php
-    // Retrieve the most recent post
-    $latest_post = get_posts( array(
-      'numberposts' => 3,
-      'orderby' => 'post_date',
-      'order' => 'DESC',
-      'post_type' => 'post',
-      'post_status' => 'publish'
-    ) );
-
-    // Loop through the posts
-    foreach ( $latest_post as $post ) {
-      // Get the featured image URL
-      $featured_image_url = get_the_post_thumbnail_url( $post->ID );
-      // Get the post title
-      $title = $post->post_title;
-      // Get the post URL
-      $post_url = get_permalink( $post->ID );
-
-      // Output the HTML structure for the post
-      ?>
-      <a href="<?php echo $post_url; ?>" class="latest-post-block-link">
-        <div class="latest-post-block">
-          <div class="latest-post-block__featured-image-container">
-            <img src="<?php echo $featured_image_url; ?>" alt="Featured image" class="latest-post-block__featured-image">
-          </div>
-          <div class="latest-post-block__title-container">
-            <h3 class="latest-post-block__title"><?php echo $title; ?></h3>
-          </div>
-        </div>
-      </a>
-    <?php } ?>
-</div>
--- wp-content/themes/blankslate/header.php
+++ wp-content/themes/blankslate/header.php
@@ -32,40 +32,6 @@ if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo ''; }
 <?php dynamic_sidebar( 'primary-widget-area' ); ?>
 </ul>
 </div>
-<div class="latest-post-block-container">
-  <?php
-    // Retrieve the latest post
-    $latest_post = get_posts( array(
-      'numberposts' => 2,
-      'orderby' => 'post_date',
-      'order' => 'DESC',
-      'post_type' => 'post',
-      'post_status' => 'publish'
-    ) );
-
-    // Loop through the posts
-    foreach ( $latest_post as $post ) {
-      // Get the featured image URL
-      $featured_image_url = get_the_post_thumbnail_url( $post->ID );
-      // Get the post title
-      $title = $post->post_title;
-      // Get the post URL
-      $post_url = get_permalink( $post->ID );
-
-      // Output the HTML structure for each post
-      ?>
-      <a href="<?php echo $post_url; ?>" class="latest-post-block-link">
-        <div class="latest-post-block">
-          <div class="latest-post-block__featured-image-container">
-            <img src="<?php echo $featured_image_url; ?>" alt="Featured image" class="latest-post-block__featured-image">
-          </div>
-          <div class="latest-post-block__title-container">
-            <h3 class="latest-post-block__title"><?php echo $title; ?></h3>
-          </div>
-        </div>
-      </a>
-    <?php } ?>
-    </div>
 </div>
 
 </aside>
