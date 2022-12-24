4664d5f975abeaf1169f939b042d724315edc8f5
s9ori <sebauchida@gmail.com>
2022-12-23 22:21:02 -0500

    reiterate

--- wp-content/themes/blankslate/homepage.php
+++ wp-content/themes/blankslate/homepage.php
@@ -61,7 +61,7 @@ $user_id = '2819050825';
 
 // Use the curl function to make a GET request to the user Tweet timeline endpoint
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, "https://api.twitter.com/2/users/$user_id/tweets?max_results=30?tweet_mode=extended");
+curl_setopt($ch, CURLOPT_URL, "https://api.twitter.com/2/users/$user_id/tweets?count=30&tweet_mode=extended");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_HTTPHEADER, array(
   "Authorization: Bearer $bearer_token",
