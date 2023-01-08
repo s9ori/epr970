e14a3231b6b3817b96fc87f1e0e88e8c0ecd1cc9
s9ori <sebauchida@gmail.com>
2023-01-08 16:18:59 -0500

    sandoll

--- wp-content/themes/blankslate/functions.php
+++ wp-content/themes/blankslate/functions.php
@@ -36,6 +36,16 @@ add_action( 'wp_footer', 'blankslate_footer' );
 function blankslate_footer() {
 ?>
 <script>
+  (function(d) {
+    var config = {
+      kitId: 'ecq4oph',
+      scriptTimeout: 3000,
+      async: true
+    },
+    h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+  })(document);
+</script>
+<script>
 jQuery(document).ready(function($) {
 var deviceAgent = navigator.userAgent.toLowerCase();
 if (deviceAgent.match(/(iphone|ipod|ipad)/)) {
