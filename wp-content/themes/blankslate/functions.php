f53c7ea28802827f56f9658110a3b672da29bc00
s9ori <sebauchida@gmail.com>
2023-01-08 20:12:57 -0500

    commit header

--- wp-content/themes/blankslate/functions.php
+++ wp-content/themes/blankslate/functions.php
@@ -59,8 +59,8 @@ for (let i = 0; i < $hoverables.length; i++) {
 // Move the cursor
 function onMouseMove(e) {
   TweenMax.to($bigBall, .4, {
-    x: e.pageX - 15,
-    y: e.pageY - 15
+    x: e.pageX - 25,
+    y: e.pageY - 25
   })
   TweenMax.to($smallBall, .1, {
     x: e.pageX - 5,
