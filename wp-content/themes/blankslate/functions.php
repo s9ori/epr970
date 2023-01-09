b172c9a416b3cbaf9f3d0797110ca6fa6900d7bc
s9ori <sebauchida@gmail.com>
2023-01-08 21:08:57 -0500

    differe

--- wp-content/themes/blankslate/functions.php
+++ wp-content/themes/blankslate/functions.php
@@ -97,11 +97,11 @@ function onMouseHoverOut() {
   TweenMax.to($bigBall, .3, {
     scale: 1,
     opacity: 1,
-    fill: 'white'
+    fill: '#00000'
   });
   $bigBall.style.mixBlendMode = 'difference';
   TweenMax.to($smallBall, .3, {
-    fill: 'black'
+    fill: '#00000'
   })
 }
 </script>
