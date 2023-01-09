e52a6ea958a0f790e0c71f340c221ee0ede11bdb
s9ori <sebauchida@gmail.com>
2023-01-09 17:59:49 -0500

    href

--- wp-content/themes/blankslate/functions.php
+++ wp-content/themes/blankslate/functions.php
@@ -48,7 +48,7 @@ function blankslate_footer() {
 <script>
 const $bigBall = document.querySelector('.cursor__ball--big');
 const $smallBall = document.querySelector('.cursor__ball--small');
-const $hoverables = document.querySelectorAll('a[href]');
+const $hoverables = document.querySelectorAll('a[href]:not(.latest-post-block__title a[href])');
 const $titleHoverables = document.querySelectorAll('.latest-post-block__title');
 
 
