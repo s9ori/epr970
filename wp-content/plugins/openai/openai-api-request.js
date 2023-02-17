d4c7e9146f35ab12ae41ca6558672fe963b8c155
s9ori <sebauchida@gmail.com>
2023-02-17 17:52:41 -0500

    researchers recently found high concentrations of fecal bacteria on sidewalks, carpets, and shoe soles. @JacJeffreyW breaks down the study and the etiquette around asking people to take their shoes off.

--- wp-content/plugins/openai/openai-api-request.js
+++ wp-content/plugins/openai/openai-api-request.js
@@ -27,7 +27,7 @@ previousResponseArray = JSON.parse(cachedResponse);
 }
 var data = {
 "model": model,
-"prompt": "Mimicing the structure, voice, and tone of these previous tweets from the Brian Lehrer Show:" + file_contents + ", give me a list of five engaging Tweets:" + prompt,
+"prompt": "Mimicing the structure, voice, and tone of these previous tweets from the Brian Lehrer Show:" + file_contents + ", here is a list of five engaging Tweets about:" + prompt,
 "max_tokens": max_tokens,
 "temperature": temperature
 };
