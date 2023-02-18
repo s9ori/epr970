613ab9b8603a7239dcfd4a63c8e7e123ffc6c2ce
s9ori <sebauchida@gmail.com>
2023-02-17 21:30:27 -0500

    <?php /* Template Name: Saori Uchida Homepage EPR970 */
    get_header();
    ?>

--- wp-content/themes/blankslate/Tweetbot.php
+++ wp-content/themes/blankslate/Tweetbot.php
@@ -1,4 +1,8 @@
-<!--Twwtbots-->
+<?php
+/*
+Template Name: Saori Uchida Homepage EPR970
+*/
+?>
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 <div class="entry-basics">
