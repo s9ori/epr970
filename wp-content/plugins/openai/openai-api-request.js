d676d9fca74d11ebecba8e19e317c77baee1eda1
s9ori <sebauchida@gmail.com>
2023-02-19 12:38:40 -0500

    Kyle Moore, economist with the Economic Policy Institute’s Program on Race, Ethnicity, and the Economy at the Economic Policy Institute (EPI), talks about persistence of, and ways to eliminate, the Black-white wealth gap which is still evident despite civil rights and fair housing legislation.

--- wp-content/plugins/openai/openai-api-request.js
+++ wp-content/plugins/openai/openai-api-request.js
@@ -199,7 +199,7 @@ $('.openai-response').css({
     var api_key = openai_data.api_key;
     var model = "text-davinci-003";
     var max_tokens = 420;
-    var temperature = .88;
+    var temperature = .7;
     var url = "https://api.openai.com/v1/completions";
     var prompt2 = previousResponseArray[previousResponseArray.length - 1]; // Get the last response from the array
 
