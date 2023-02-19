ce04f7b9d4cdb274b74a4bf9e5183bf5a1b07378
s9ori <sebauchida@gmail.com>
2023-02-19 13:38:58 -0500

    Robert Jackson, NYS Senator (D 31st, West & Upper Manhattan), talks about his proposed legislation to end qualified immunity for police officers and other public officials in New York State -- a bill that Governor Hochul opposes.

--- wp-content/themes/blankslate/Tweetbot.php
+++ wp-content/themes/blankslate/Tweetbot.php
@@ -5,7 +5,6 @@ Template Name: Saori Uchida Homepage EPR970
 
 get_header();
 ?>
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 <div class="entry-basics">
 <h1 class="entry-title" itemprop="name"><?php the_title(); ?></h1> <?php edit_post_link(); ?>
@@ -14,7 +13,7 @@ get_header();
 <div class="landing-container">
     <div class="landing">
     <div class="texted">
-    <h1>nævis... write me some <strong>hit</strong> tweets about:</h1>
+    <h1>nævis... write me some <strong>hit baby</strong> tweets about:</h1>
     </div>
     <div class="landing-frame">[openai_api_request_form]</div>
     </div>
@@ -31,6 +30,4 @@ get_header();
     </div>
 </div>
 </article>
-<?php if ( comments_open() && !post_password_required() ) { comments_template( '', true ); } ?>
-<?php endwhile; endif; ?>
 <?php get_footer(); ?>
