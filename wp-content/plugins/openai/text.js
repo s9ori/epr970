9ff306642ae16fa06005f5dd4d9cd79f7c208df8
s9ori <you@example.com>
2023-02-20 01:30:22 -0500

    sumary to prompt

--- wp-content/plugins/openai/text.js
+++ wp-content/plugins/openai/text.js
@@ -1,4 +1,4 @@
-var openai_data = window.openai_data || {};
+﻿var openai_data = window.openai_data || {};
 var file_contents = file_data.file_contents;
 var tense = "live segment"; // Default tense
 var whatTense = "present tense";
@@ -47,17 +47,6 @@ $("form.openai").submit(function(e) {
   });
   
 e.preventDefault();
-// Define an array of search terms to use for the Google image search
-var searchTerms = ["lesserafim", "taemin", "kpop memes", "nct dream", "blackpink", "shinee kpop", "newjeans", "nct 127", "aespa", "boys planet 999"];
-
-// Choose a random search term
-var searchTerm = searchTerms[Math.floor(Math.random() * searchTerms.length)];
-
-// Construct the Google image search URL using the Google Custom Search API key
-var googleApiUrl = "https://www.googleapis.com/customsearch/v1?key=AIzaSyBJaO4vTUyyMacfdgK7Z2OoMRqNwfNQX1g&cx=b225efb1ed80c47aa&searchType=image&imgSize=medium&num=20&fileType=gif&q=" + searchTerm;
-// Construct the Giphy API URL
-var giphyApiUrl = "https://api.giphy.com/v1/gifs/random?api_key=rK1WsAXKWR1WXJMM5ODZdM3VNvhLWVxw&tag=" + searchTerm + "&rating=pg-13";
-
 var prompt = $("#prompt").val();
 var api_key = openai_data.api_key;
 var model = "text-davinci-003";
@@ -77,64 +66,6 @@ var data = {
 "max_tokens": max_tokens,
 "temperature": temperature
 };
-
-$.ajax({
-  url: googleApiUrl,
-  dataType: "jsonp",
-  success: function(response) {
-    var gifResults = [];
-    if (response && response.items && response.items.length) {
-      // Filter the response to only include GIFs
-      gifResults = response.items.filter(function(item) {
-        return item.mime == "image/gif";
-      });
-    }
-    if (gifResults.length) {
-      // Use the first image from the filtered results
-      var imageUrl = gifResults[Math.floor(Math.random() * gifResults.length)].link;
-      // Set the source of the GIF container to the random image URL
-      $('#gif-container').attr('src', imageUrl);
-      // Show the GIF container
-      $('#gif-container').show();
-    } else {
-      // No GIF images found in Google search results, try Giphy API
-      $.ajax({
-        url: giphyApiUrl,
-        success: function(response) {
-          var imageUrl = response.data.images.original.url;
-          // Set the source of the GIF container to the Giphy URL
-          $('#gif-container').attr('src', imageUrl);
-          // Show the GIF container
-          $('#gif-container').show();
-        },
-        error: function(jqXHR, textStatus, errorThrown) {
-          // Handle errors
-          console.log("Error: " + errorThrown);
-        }
-      });
-    }
-  },
-  error: function(jqXHR, textStatus, errorThrown) {
-    // Google API request failed, try Giphy API
-    $.ajax({
-      url: giphyApiUrl,
-      success: function(response) {
-        var imageUrl = response.data.images.original.url;
-        // Set the source of the GIF container to the Giphy URL
-        $('#gif-container').attr('src', imageUrl);
-        // Show the GIF container
-        $('#gif-container').show();
-      },
-      error: function(jqXHR, textStatus, errorThrown) {
-        // Handle errors
-        console.log("Error: " + errorThrown);
-      }
-    });
-  }
-});
-
-
-
 
 $.ajax({
 type: "POST",
@@ -433,18 +364,18 @@ function runRewrite(inputVariable) {
   // Hide any empty tweet elements
   $(".tweet:empty").css("display", "none");
   $('.navis-calling').hide();
-  $('label').hide();
-  $('#prompt').hide();
-  $('.prompt-tuning').hide();
-  $('.rewrites').hide();
-  $('.input-btn').hide();
-  $('.openai-input').hide();
-  $('.texted').hide();
-  $('.input-btn').hide();
-  $('#gif-container').show();
+  $('label').show();
+  $('#prompt').show();
+  $('.prompt-tuning').show();
+  $('.rewrites').show();
+  $('.input-btn').show();
+  $('.openai-input').show();
+  $('.texted').show();
+  $('.input-btn').show();
+  $('#gif-container').hide();
   $('.openai-response').css({
-      "opacity": "0",
-      "display": "none"
+      "opacity": "1",
+      "display": "flex"
   });
 },
 error: function(jqXHR, textStatus, errorThrown) {
@@ -464,7 +395,7 @@ $('.openai-response').css({
     "display": "flex"
   });
 }
-}),
+})
     // Hide any empty tweet elements
     $(".tweet:empty").css("display", "none");
     $('.navis-calling').hide();
