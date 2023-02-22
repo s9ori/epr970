b360ebfec64091a01af307944da5732bb9436433
s9ori <sebauchida@gmail.com>
2023-02-21 23:57:29 -0500

    prep

---
+++ wp-content/plugins/openai/prep.php
@@ -0,0 +1,28 @@
+﻿<?php
+// Enqueue the JS file that will make the API request
+function openai_api_request_form_enqueue_scripts() {
+  wp_enqueue_script( 'jquery' );
+  wp_enqueue_script( 'openai-api-request', plugin_dir_url( __FILE__ ) . '/prompt.js', array( 'jquery' ), '1.0', true );
+  $data = array(
+    'api_key' => getenv('API_KEY')
+  );
+  wp_localize_script( 'openai-api-request', 'openai_data', $data );
+  $file_contents = file_get_contents( plugin_dir_path( __FILE__ ) . 'prompt.json' );
+  $prompt = json_decode( $file_contents );
+  wp_localize_script( 'openai-api-request', 'file_data', array( 'prompt' => $prompt ) );
+}
+
+// Display the form that will trigger the API request
+function openai_api_request_form() {
+  ?>
+  <form id="prep-form">
+    <div>
+      <label for="prompt">Prompt:</label>
+      <textarea name="prompt" id="prompt"></textarea>
+    </div>
+    <div>
+      <button type="submit">Make API Request</button>
+    </div>
+  </form>
+  <?php
+}
