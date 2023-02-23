1efe03349b18b8967835c7fe5c15fbc32c64da65
s9ori <sebauchida@gmail.com>
2023-02-23 10:38:42 -0500

    // Update DOM elements with current state levels   document.getElementById("mood-state").textContent = pet.mood;   document.getElementById("fitness-state").textContent = pet.fitness;   document.getElementById("power-level").textContent = "Power Level: " + pet.powerLevel; }

--- wp-content/plugins/pet/main.js
+++ wp-content/plugins/pet/main.js
@@ -5,71 +5,73 @@ const exerciseButton = document.getElementById("exercise");
 
 // Define mood states
 const MOOD_STATES = {
-    SLEEPY: "sleepy",
-    HAPPY: "happy",
-    PENSIVE: "pensive",
-    SASSY: "sassy",
-    EXCITED: "excited",
-    JOYFUL: "joyful"
+  SLEEPY: "sleepy",
+  HAPPY: "happy",
+  PENSIVE: "pensive",
+  SASSY: "sassy",
+  EXCITED: "excited",
+  JOYFUL: "joyful"
 };
 
 // Define fitness states
 const FITNESS_STATES = {
-    HUNGRY: "hungry",
-    CUTTING: "cutting",
-    FIT: "fit",
-    BULKING: "bulking",
-    FULL_BELLY: "full belly"
+  HUNGRY: "hungry",
+  CUTTING: "cutting",
+  FIT: "fit",
+  BULKING: "bulking",
+  FULL_BELLY: "full belly"
 };
 
 // Define pet object with default values
 let pet = {
-    mood: MOOD_STATES.HAPPY,
-    fitness: FITNESS_STATES.FIT,
-    powerLevel: 0
+  mood: MOOD_STATES.HAPPY,
+  fitness: FITNESS_STATES.FIT,
+  powerLevel: 0
 };
 
 // Define point values for interactions
 const INTERACTION_POINTS = {
-    PLAY: {
-        mood: 10,
-        powerLevel: 5
-    },
-    FEED: {
-        fitness: -5,
-        mood: 1,
-        powerLevel: 1
-    },
-    EXERCISE: {
-        fitness: 10,
-        mood: -5,
-        powerLevel: 5
-    }
+  PLAY: {
+    mood: 10,
+    powerLevel: 5
+  },
+  FEED: {
+    fitness: -5,
+    mood: 1,
+    powerLevel: 1
+  },
+  EXERCISE: {
+    fitness: 10,
+    mood: -5,
+    powerLevel: 5
+  }
 };
 
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
 
-playButton.addEventListener("click", playWithPet);
-feedButton.addEventListener("click", feedPet);
-exerciseButton.addEventListener("click", exercisePet);
+  playButton.addEventListener("click", playWithPet);
+  feedButton.addEventListener("click", feedPet);
+  exerciseButton.addEventListener("click", exercisePet);
 
-window.addEventListener("load", () => {
+  window.addEventListener("load", () => {
     retrievePetState();
     updatePetState(pet);
-});
+  });
 
-setInterval(() => {
+  setInterval(() => {
     updateMoodState();
     updatePowerLevel();
     updatePetState(pet);
     savePetState();
-}, 10000);
+  }, 10000);
 
-function updateMoodState() {
-    pet.mood += INTERACTION_POINTS.PLAY.mood || 0;
-    pet.mood += INTERACTION_POINTS.FEED.mood || 0;
-    pet.mood += INTERACTION_POINTS.EXERCISE.mood || 0;
-    
+  function updateMoodState() {
+    let moodDelta = 0;
+    moodDelta += INTERACTION_POINTS.PLAY.mood || 0;
+    moodDelta += INTERACTION_POINTS.FEED.mood || 0;
+    moodDelta += INTERACTION_POINTS.EXERCISE.mood || 0;
+    pet.mood += moodDelta;
+
     if (pet.mood >= 80) {
       pet.mood = MOOD_STATES.JOYFUL;
     } else if (pet.mood >= 60) {
@@ -82,9 +84,15 @@ function updateMoodState() {
       pet.mood = MOOD_STATES.SLEEPY;
     }
     document.getElementById("mood-state").textContent = pet.mood;
-}
-  
+  }
+
   function updatePowerLevel() {
+    let powerLevelDelta = 0;
+    powerLevelDelta += INTERACTION_POINTS.PLAY.powerLevel || 0;
+    powerLevelDelta += INTERACTION_POINTS.FEED.powerLevel || 0;
+    powerLevelDelta += INTERACTION_POINTS.EXERCISE.powerLevel || 0;
+    pet.powerLevel += powerLevelDelta;
+
     if (pet.powerLevel >= 80) {
       pet.powerLevel = "supercharged";
     } else if (pet.powerLevel >= 60) {
@@ -104,6 +112,10 @@ function updateMoodState() {
 function updatePetState(newState) {
     pet = { ...pet, ...newState };
     localStorage.setItem("petState", JSON.stringify(pet));
+ // Update DOM elements with current state levels
+ document.getElementById("mood-state").textContent = pet.mood;
+ document.getElementById("fitness-state").textContent = pet.fitness;
+ document.getElementById("power-level").textContent = "Power Level: " + pet.powerLevel;
 }
 
 // Define function to retrieve pet state from local storage
@@ -112,6 +124,10 @@ function retrievePetState() {
     if (storedPetState) {
         pet = JSON.parse(storedPetState);
     }
+ // Update DOM elements with current state levels
+ document.getElementById("mood-state").textContent = pet.mood;
+ document.getElementById("fitness-state").textContent = pet.fitness;
+ document.getElementById("power-level").textContent = "Power Level: " + pet.powerLevel;
 }
 
 function playWithPet() {
