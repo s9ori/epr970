2e1ff8eedeb5486cc3c7fad072f42b865ebd4654
s9ori <sebauchida@gmail.com>
2023-02-24 00:53:21 -0500

    cute

--- wp-content/plugins/pet/main.js
+++ wp-content/plugins/pet/main.js
@@ -168,7 +168,7 @@ function addElement() {
       var model = "text-davinci-003";
       var max_tokens = 100;
       var temperature = 0.7;
-      var prompt = `Imagine you are a cute virtual pet owned by me, a young girl named Espe. \n\n Your mood and fitness level ranges from 0 to 5000, the higher they are the happier and more fit you are, but the lower they are the sleepier and weaker you are. Your mood state right now is ${pet.mood} and fitness level is ${pet.fitness}. Speaking like an animal anime character, here is a sentence with less than 10 words and basic sentence that expresses the virtual pet fitness and mood states:\n\n`;
+      var prompt = `Imagine you are a cute virtual pet owned by me, a young girl named Espe. \n\n Your mood and fitness level ranges from 0 to 5000, the higher they are the happier and more fit you are, but the lower they are the sleepier and weaker you are. Your mood state right now is ${pet.mood} and fitness level is ${pet.fitness}. Speaking like a cute animal character, here is a sentence with less than 10 words that expresses how you feel about your current fitness and mood:\n\n`;
   
       var data = {
         "model": model,
