343e2c390d2ef7aa1bc76c873e0a12caf5d303e9
s9ori <sebauchida@gmail.com>
2023-02-23 22:44:18 -0500

    return

--- wp-content/plugins/pet/main.js
+++ wp-content/plugins/pet/main.js
@@ -8,49 +8,29 @@ const exerciseButton = document.getElementById("exercise");
 let pet = {
   mood: 25,
   fitness: 25,
-  powerPoints: 0
+  powerLevel: 0
 };
 
 // Define point values for interactions
 const INTERACTION_POINTS = {
   PLAY: {
     mood: 10,
-    powerPoints: () => getRandompowerPoints()
+    powerLevel: () => getRandomPowerLevel()
 
   },
   FEED: {
     fitness: -5,
     mood: 5,
-    powerPoints: 5
+    powerLevel: 5
   },
   EXERCISE: {
     fitness: 10,
     mood: -5,
-    powerPoints: 10
+    powerLevel: 10
   }
 };
 
-// Define function to calculate experience required for a given level
-function getExperienceForLevel(level) {
-    if (level <= 1) {
-      return 0;
-    } else {
-      return Math.floor(getExperienceForLevel(level - 1) + (level - 1) + 300 * Math.pow(2, (level - 1) / 7));
-    }
-  }
-  
-  // Define function to get the pet's current level based on its power level
-  function getLevel(powerPoints) {
-    let level = 1;
-    let experience = 0;
-    while (level <= LEVEL_CAP && experience <= powerPoints) {
-      experience += getExperienceForLevel(level);
-      level++;
-    }
-    return level - 1;
-  }
-
-function getRandompowerPoints() {
+function getRandomPowerLevel() {
     return Math.floor(Math.random() * 2) * 5;
   }
 
@@ -88,29 +68,29 @@ function decreaseMoodAndFitness() {
     document.getElementById("mood-state").textContent = moodState;
   }
 
-  function updatepowerPoints() {
-    let powerPointsDelta = 0;
-    powerPointsDelta += parseInt(INTERACTION_POINTS.PLAY.powerPoints) || 0;
-    powerPointsDelta += parseInt(INTERACTION_POINTS.FEED.powerPoints) || 0;
-    powerPointsDelta += parseInt(INTERACTION_POINTS.EXERCISE.powerPoints) || 0;
-    pet.powerPoints += powerPointsDelta;
+  function updatePowerLevel() {
+    let powerLevelDelta = 0;
+    powerLevelDelta += parseInt(INTERACTION_POINTS.PLAY.powerLevel) || 0;
+    powerLevelDelta += parseInt(INTERACTION_POINTS.FEED.powerLevel) || 0;
+    powerLevelDelta += parseInt(INTERACTION_POINTS.EXERCISE.powerLevel) || 0;
+    pet.powerLevel += powerLevelDelta;
   
-    document.getElementById("power-level").textContent = powerPointsState;
+    document.getElementById("power-level").textContent = powerLevelState;
   }
 
   setInterval(() => {
-    const powerPoints = pet.powerPoints;
-    const storedpowerPoints = parseInt(localStorage.getItem("powerPoints"));
+    const powerLevel = pet.powerLevel;
+    const storedPowerLevel = parseInt(localStorage.getItem("powerLevel"));
     
-    if (powerPoints % 50 === 0 && powerPoints !== storedpowerPoints) {
-      localStorage.setItem("powerPoints", powerPoints);
+    if (powerLevel % 50 === 0 && powerLevel !== storedPowerLevel) {
+      localStorage.setItem("powerLevel", powerLevel);
   
       // Make AJAX call to OpenAI API
       var api_key = openai_data2.api_key;
       var model = "text-davinci-003";
       var max_tokens = 200;
       var temperature = 0.8;
-      var prompt = `Imagine you are a cute virtual pet owned by a young radio producer names Espe. \n\n As her pet, you must learn about Boys Planet 999, the South Korean boy group reality survival show to form a global pop music boy group, from this fan discussion on the latets episode: \n" ${file_contents2}. \n\n Your mood and fitness levels change when Espe interacts with you. \n\nYour mood and fitness level ranges from 0 to 1000, the higher they are the happier and more fit you are. Your mood state right now is ${pet.mood} and fitness level is ${pet.fitness}. Speaking like a kawaii anime animal character, here are ten words or less that express the virtual pet fitness and mood states and subtly references Boy Planet 999:\n\n`;
+      var prompt = `Imagine you are a cute virtual pet owned by a young radio producer names Espe. \n\n As her pet, you must learn about Boys Planet 999, the South Korean boy group reality survival show to form a global pop music boy group, from this fan discussion on the latets episode: \n" ${file_contents2}. \n\n Your mood and fitness levels change when Espe interacts with you. \n\nYour mood and fitness level ranges from 0 to 1000, the higher they are the happier and more fit you are. Your mood state right now is ${pet.mood} and fitness level is ${pet.fitness}. Speaking like a kawaii anime animal character, here are ten words or less that express the virtual pet fitness and mood states and references Boy Planet 999:\n\n`;
   
       var data = {
         "model": model,
@@ -141,16 +121,11 @@ function decreaseMoodAndFitness() {
 // Define function to update pet state and save to local storage
 function updatePetState(newState) {
     pet = { ...pet, ...newState };
-    const newLevel = getLevel(pet.powerPoints);
-    if (newLevel !== pet.level) {
-        pet.level = newLevel;
-        document.getElementById("level").textContent = `Level: ${pet.level}`;
-      }
     localStorage.setItem("petState", JSON.stringify(pet));
  // Update DOM elements with current state levels
  document.getElementById("mood-state").textContent = pet.mood;
  document.getElementById("fitness-state").textContent = pet.fitness;
- document.getElementById("power-level").textContent = pet.powerPoints;
+ document.getElementById("power-level").textContent = pet.powerLevel;
 }
 
 // Define function to retrieve pet state from local storage
@@ -162,25 +137,25 @@ function retrievePetState() {
  // Update DOM elements with current state levels
  document.getElementById("mood-state").textContent = pet.mood;
  document.getElementById("fitness-state").textContent = pet.fitness;
- document.getElementById("power-level").textContent = pet.powerPoints;
+ document.getElementById("power-level").textContent = pet.powerLevel;
 }
 
 function playWithPet() {
     pet.mood += parseInt(INTERACTION_POINTS.PLAY.mood) || 0;
-    pet.powerPoints += parseInt(INTERACTION_POINTS.PLAY.powerPoints()) || 0;
+    pet.powerLevel += parseInt(INTERACTION_POINTS.PLAY.powerLevel()) || 0;
     updatePetState(pet);
 }
 function feedPet() {
     pet.fitness += parseInt(INTERACTION_POINTS.FEED.fitness) || 0;
     pet.mood += parseInt(INTERACTION_POINTS.FEED.mood) || 0;
-    pet.powerPoints += parseInt(INTERACTION_POINTS.FEED.powerPoints) || 0;    
+    pet.powerLevel += parseInt(INTERACTION_POINTS.FEED.powerLevel) || 0;    
     updatePetState(pet);
 }
 
 function exercisePet() {
     pet.fitness += parseInt(INTERACTION_POINTS.EXERCISE.fitness) || 0;
     pet.mood += parseInt(INTERACTION_POINTS.EXERCISE.mood) || 0;
-    pet.powerPoints += parseInt(INTERACTION_POINTS.EXERCISE.powerPoints) || 0;  
+    pet.powerLevel += parseInt(INTERACTION_POINTS.EXERCISE.powerLevel) || 0;  
     updatePetState(pet);
 }
 
