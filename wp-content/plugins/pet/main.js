d347c5eb79ed0522dbc9484c014bd1f8d1ee6bf5
s9ori <sebauchida@gmail.com>
2023-02-23 22:06:22 -0500

    const LEVEL_CAP = 99;

--- wp-content/plugins/pet/main.js
+++ wp-content/plugins/pet/main.js
@@ -11,6 +11,8 @@ let pet = {
   powerPoints: 0
 };
 
+const LEVEL_CAP = 99;
+
 // Define point values for interactions
 const INTERACTION_POINTS = {
   PLAY: {
@@ -110,7 +112,7 @@ function decreaseMoodAndFitness() {
       var model = "text-davinci-003";
       var max_tokens = 200;
       var temperature = 0.8;
-      var prompt = `Imagine you are a cute virtual pet owned by a young radio producer names Espe. \n\n As her pet, you must learn about Boys Planet 999, the South Korean boy group reality survival show to form a global pop music boy group, from this fan discussion on the latets episode: \n" ${file_contents2}. \n\n Your mood and fitness levels change when Espe interacts with you. \n\nYour mood and fitness level ranges from 0 to 1000, the higher they are the happier and more fit you are. Your mood state right now is ${pet.mood} and fitness level is ${pet.fitness}. Speaking like a kawaii anime animal character, here are ten words or less that express the virtual pet fitness and mood states and references Boy Planet 999:\n\n`;
+      var prompt = `Imagine you are a cute virtual pet owned by a young radio producer names Espe. \n\n As her pet, you must learn about Boys Planet 999, the South Korean boy group reality survival show to form a global pop music boy group, from this fan discussion on the latets episode: \n" ${file_contents2}. \n\n Your mood and fitness levels change when Espe interacts with you. \n\nYour mood and fitness level ranges from 0 to 5000, the higher they are the happier and more fit you are. Your mood state right now is ${pet.mood} and fitness level is ${pet.fitness}. Speaking like a baby animal character, here are ten words or less that express the virtual pet fitness and mood states and subtly references Boy Planet 999:\n\n`;
   
       var data = {
         "model": model,
