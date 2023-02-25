0b79ab43c7e8796cd3516b84c577e6971fa4546b
s9ori <sebauchida@gmail.com>
2023-02-24 21:29:23 -0500

    monsters

--- wp-content/themes/blankslate/header.php
+++ wp-content/themes/blankslate/header.php
@@ -53,6 +53,7 @@ if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo ''; }
 <img class="logo" src="https://lowfemme.com/wp-content/uploads/2023/02/tumblr_neqyicWGSs1u1nuzeo1_500.gif" alt="Pet Image"></a>
 <div id="response"></div>
 </div>
+<div id="monsters"></div>
 <div id="foods"></div>
 <nav id="menu" role="navigation" itemscope itemtype="https://schema.org/SiteNavigationElement">
 <?php wp_nav_menu( array( 'theme_location' => 'main-menu', 'link_before' => '<span itemprop="name">', 'link_after' => '</span>' ) ); ?>
