0bcd0cc46b050fef7fe52d15e17f6517d691d4ff
s9ori <sebauchida@gmail.com>
2023-02-25 14:26:30 -0500

    dae

--- wp-content/plugins/pet/main.js
+++ wp-content/plugins/pet/main.js
@@ -11,6 +11,17 @@ window.addEventListener("load", () => {
     levelElement.textContent = `Level: ${pet.level}`;
     updateLevel();
     const savedDrops = localStorage.getItem("drops");
+    let fitnessBoost, imageSrc;
+  if (type === "ribbon1") {
+    fitnessBoost = 300;
+    imageSrc = ' https://lowfemme.com/wp-content/uploads/2023/02/tumblr_2dd2dd3e0bc9407e8e0d1a3b01c67b38_4b38d417_75.webp';
+  } else if (type === "ribbon2") {
+    fitnessBoost = 600;
+    imageSrc = 'https://lowfemme.com/wp-content/uploads/2023/02/tumblr_bd16179ec8017844f4175a144f1b6a2c_5a6b991f_75.webp';
+  } else if (type === "ribbon3") {
+    fitnessBoost = 900;
+    imageSrc = 'https://lowfemme.com/wp-content/uploads/2023/02/tumblr_9acc2ace0bf9920ded8a4ef9a1be77ee_c56fb2cd_75.webp';
+  }
     if (savedDrops) {
       dropsDiv.innerHTML = savedDrops;
       const drops = dropsDiv.querySelectorAll(".drop");
@@ -25,7 +36,7 @@ window.addEventListener("load", () => {
     }
   });
 
-  const interactBtns = document.querySelectorAll('.interaction button');
+const interactBtns = document.querySelectorAll('.interaction button');
 
 interactBtns.forEach((btn) => {
   btn.addEventListener('click', () => {
@@ -42,7 +53,6 @@ interactBtns.forEach((btn) => {
   });
 });
 
-  
 const levelElement = document.getElementById("level");
 const adventureButton = document.getElementById("adventure");
 const foodsDiv = document.getElementById("foods");
