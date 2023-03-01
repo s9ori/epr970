2f5fb5273d449feac5657a6897c87aaf323977a9
s9ori <sebauchida@gmail.com>
2023-03-01 17:53:33 -0500

    openai

--- wp-content/plugins/openai/openai-api-request.js
+++ wp-content/plugins/openai/openai-api-request.js
@@ -44,10 +44,10 @@ $("form.openai").submit(function(e) {
 
   if ($('#summarizeArticle').is(':checked')) {
       var api_key = openai_data1.api_key;
-      var model = "gpt-3.5-turbo";
+      var model = "text-davinci-003";
       var max_tokens = 500;
       var temperature = .7;
-      var url = "https://api.openai.com/v1/chat/completions";
+      var url = "https://api.openai.com/v1/completions";
       var prompt = $("#prompt").val();
       var author = $("#author").val();
     
@@ -83,10 +83,10 @@ $("form.openai").submit(function(e) {
       success: function(result) {
         var text1 = result.choices[0].text;
         var api_key = openai_data1.api_key;
-        var model = "gpt-3.5-turbo";
+        var model = "text-davinci-003";
         var max_tokens = 420;
         var temperature = .7;
-        var url = "https://api.openai.com/v1/chat/completions";
+        var url = "https://api.openai.com/v1/completions";
         var cacheKey = prompt; // Only use the prompt to construct the cache key
         cacheCounter++; // Increment the counter
         var cachedResponse = localStorage.getItem(cacheKey);
@@ -198,10 +198,10 @@ $("form.openai").submit(function(e) {
     else {
       var prompt = $("#prompt").val();
       var api_key = openai_data1.api_key;
-      var model = "gpt-3.5-turbo";
+      var model = "text-davinci-003";
       var max_tokens = 420;
       var temperature = .82;
-      var url = "https://api.openai.com/v1/chat/completions";
+      var url = "https://api.openai.com/v1/completions";
       var cacheKey = prompt; // Only use the prompt to construct the cache key
       var author = $("#author").val();
 
@@ -315,9 +315,9 @@ function runRewrite(inputVariable) {
     var cacheKey = $('#prompt').val() + '-' + (cacheCounter - 1); // Get the cache key from the previous request
     var api_key = openai_data1.api_key;
     var max_tokens = 800;
-    var model = "gpt-3.5-turbo";
+    var model = "text-davinci-003";
     var temperature = .6;
-    var url = "https://api.openai.com/v1/chat/completions";
+    var url = "https://api.openai.com/v1/completions";
     var prompt2 = previousResponseArray[previousResponseArray.length - 1]; // Get the last response from the array
     var prompt3 = $("#prompt").val();
 
